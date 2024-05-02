c0a87bef27b46a741e575219e5c903e4100ed6ee
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-02 20:06:25 +0900

    Time: 64 ms (48.79%), Space: 17.8 MB (16.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,41 @@
+#브루트 포스
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i]+nums[j] == target:
+#                     return [i,j]
+
+#in 이용 탐색
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i, n in enumerate(nums):
+#             complement = target-n
+
+#             if complement in nums[i+1:]:
+#                 return [nums.index(n), nums[i+1:].index(complement)+(i+1)]
+
+#첫 번째 수를 뺀 결과 키 조회
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         nums_map = {}
+#         for i, num in enumerate(nums):
+#             nums_map[num] = i
+#         for i, num in enumerate(nums):
+#             if target-num in nums_map and i!= nums_map[target-num]:
+#                 return [i, nums_map[target-num]]
+
+# 조회 구조 개선
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        nums_map = {}
+
+        for i, num in enumerate(nums):
+            if target-num in nums_map:
+                return [nums_map[target-num], i]
+            nums_map[num] = i
+
+
+
 
         
