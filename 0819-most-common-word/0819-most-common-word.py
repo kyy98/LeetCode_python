49e67680145666d9b9bc1d4bc12ab2f7c8c12ac5
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-16 10:09:21 +0900

    Update 0819-most-common-word.py

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,10 +1,10 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        s = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+        s = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]   ##[0-9a-zA-Z]
 
         counts = collections.Counter(s)
 
-        return counts.most_common(1)[0][0]
+        return counts.most_common(1)[0][0]       ## [(word: count)] -> (word, count) -> word
 
 
         
