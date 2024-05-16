803dc0942d77318b160f0469a7585a1f80f3bd04
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-16 14:46:47 +0900

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -36,18 +36,18 @@
 #             nums_map[num] = i
 
 ##투포인터
+##[3,2,4] 경우 실행 안됨
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left, right = 0, len(nums) - 1
-        while not left == right:
-            # 합이 타겟보다 작으면 왼쪽 포인터를 오른쪽으로
-            if nums[left] + nums[right] < target:
-                left += 1
-            # 합이 타겟보다 크면 오른쪽 포인터를 왼쪽으로
-            elif nums[left] + nums[right] > target:
-                right -= 1
+        start, end = 0,len(nums)-1
+
+        while start != end:
+            if nums[start] + nums[end] < target:
+                start+=1
+            elif nums[start] + nums[end] > target:
+                end-=1
             else:
-                return [left, right]
+                return [start, end]
 
 
 
