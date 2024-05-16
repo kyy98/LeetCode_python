1c956120e2cfc9fc60117532252e6dbb644cb272
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-16 14:22:52 +0900

    Time: 67 ms (98.56%), Space: 16.5 MB (84.33%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:  # while 조건
+                left-=1
+                right+=1
+            return s[left+1:right]
+
+        if len(s) < 2 or s == s[::-1]:      # 예외
+            return s
+
+        longest = ''
+        for i in range(len(s)-1):
+            longest = max(longest, expand(i, i+1), expand(i,i+2), key=len) # max(str, key=len)
+
+        return longest
+        
+
+
+
+     
