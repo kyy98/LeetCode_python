c6bf93f95e54502d2987ec3a850e0a33dc93d5fe
YooBao <164113940+kyy98@users.noreply.github.com>
2024-05-16 10:23:19 +0900

    Time: 87 ms (69.68%), Space: 20 MB (63.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = collections.defaultdict(list)
+        anagrams = collections.defaultdict(list)  ## 초기값이 list 형태로 []가 주어진다.
 
-        for word in strs:
-            anagrams[''.join(sorted(word))].append(word)   ## 예를들면 "eat"을 sort하면 "aet", "tea"도 동일
-        return list(anagrams.values())
+        for w in strs:
+            anagrams[''.join(sorted(w))].append(w)
+        return anagrams.values() 
+            
+
+            
+
 
         
